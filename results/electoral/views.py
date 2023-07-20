111259eaf66c3906a977ab2fef36eebd91dbb76d
ronjonson <neillaurenceortaliz@gmail.com>
2023-07-20 16:25:18 +0800

    Converted views from function based views to class based views

--- results/electoral/views.py
+++ results/electoral/views.py
@@ -1,135 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 from . import util
 
-def index(request):
-    return render(request, "electoral/index.html")
-
-def result_2004(request):
-
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(2004, 'Pres')
-
-    return render(request, "electoral/election.html", {
-        'year': 2004,
-        'maps': maps,
-        'points': points,
-        'colors': colors,
-        'candidates': candidates,
-        'electors': electors,
-        'ranked': ranked_totals,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
-
-def result_Pres_2010(request):
-
-    year = 2010
-    position = 'Pres'
-
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
-
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'colors': colors,
-        'candidates': candidates,
-        'electors': electors,
-        'ranked': ranked_totals,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
-
-def result_VicePres_2010(request):
-
-    year = 2010
-    position = 'VicePres'
-
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
-
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'colors': colors,
-        'candidates': candidates,
-        'electors': electors,
-        'ranked': ranked_totals,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
-
-def result_Pres_2016(request):
-    year = 2016
-    position = 'Pres'
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
-
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'colors': colors,
-        'candidates': candidates,
-        'electors': electors,
-        'ranked': ranked_totals,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
+class IndexView(TemplateView):
+    template_name = 'electoral/index.html'
 
 
-def result_VicePres_2016(request):
-    year = 2016
-    position = 'VicePres'
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
 
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'colors': colors,
-        'candidates': candidates,
-        'ranked': ranked_totals,
-        'ranked_votes': ranked_votes,
-        'electors': electors,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
+class ResultView(TemplateView):
+    template_name="electoral/election.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-def result_Pres_2022(request):
-    year = 2022
-    position = 'Pres'
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'ranked': ranked_totals,
-        'colors': colors,
-        'electors': electors,
-        'candidates': candidates,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
-def result_VicePres_2022(request):
-    year = 2022
-    position = 'VicePres'
-    maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(year, position)
+        maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(kwargs["year"], kwargs["position"])
 
-    return render(request, "electoral/election.html", {
-        'year': year,
-        'maps': maps,
-        'points': points,
-        'ranked': ranked_totals,
-        'colors': colors,
-        'electors': electors,
-        'candidates': candidates,
-        'ranked_votes': ranked_votes,
-        'total_electoral_points': total_electoral_points,
-        'winning_electoral_points': winning_electoral_points
-    })
+        context["year"] = kwargs["year"]
+        context["maps"] = maps
+        context["points"] = points
+        context["ranked"] = ranked_totals
+        context["total_electoral_points"] = total_electoral_points
+        context["winning_electoral_points"] = winning_electoral_points
+        context["ranked_votes"] = ranked_votes
+        context["colors"] = colors
+        context["candidates"] = candidates
+        context["electors"] = electors
+                
+        return context
