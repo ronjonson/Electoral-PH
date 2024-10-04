c85538de4be5c9e5cff28478286428ccf0a8cf6d
Neil Ortaliz <neil.ortaliz@xurpas.com>
2024-10-04 15:45:24 +0800

    hrng maps

--- rendered/image.py
+++ rendered/image.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 
 from template_loader import template
+from renderer import HTMLPage, HTMLDiv, HTMLText, HTMLSVG
 
 save_dir = r'C:\Users\Neil\Documents\Projects\Electoral-PH\Electoral-PH\rendered\rendered_pages'
 file_name= 'pres_2004.html'
 
-def 
+def create_winner_box():
+    body = [HTMLDiv(
+        div_class="container text-center",
+        content = HTMLText(
+            text="<h1>2004 Presidential Election</h1>"
+        )
+    ),
+    HTMLDiv(
+        div_class="container-md"
+        
+    )
+    ]
 
 def main_page():
     main_page = template('base.html')
