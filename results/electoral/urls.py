5d8c1f5a24921fce403299f825799c891e2bf17c
ronjonson <neillaurenceortaliz@gmail.com>
2024-11-02 14:43:54 +0800

    testing

--- results/electoral/urls.py
+++ results/electoral/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:year>/President", views.ResultView.as_view(),{'position': 'Pres'}, name='President-Result'),
-    path("<int:year>/VicePresident", views.ResultView.as_view(), {'position': 'VicePres'}, name='VicePresident-Result'),       
+    path("<int:year>/VicePresident", views.ResultView.as_view(), {'position': 'VicePres'}, name='VicePresident-Result'),      
+    path("<int:year>/IMG/President", views.ResultImgView.as_view(), {'position': 'Pres'}, name='President-IMG-Result'),
+    path("<int:year>/IMG/VicePresident", views.ResultImgView.as_view(), {'position': 'VicePres'}, name='President-IMG-Result')   
 ]
--- results/electoral/views.py
+++ results/electoral/views.py
@@ -28,3 +28,26 @@ class ResultView(TemplateView):
         context["electors"] = electors
                 
         return context
+    
+class ResultImgView(TemplateView):
+    template_name="electoral/image.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        maps, points, ranked_totals,total_electoral_points, winning_electoral_points, ranked_votes, colors, candidates, electors = util.get_all_data(kwargs["year"], kwargs["position"])
+        context["highlight_colors"] = ['#FFD700', '#F0E68C'] 
+        context["year"] = kwargs["year"]
+        context["position"] = kwargs["position"]
+        context["maps"] = maps
+        context["points"] = points
+        context["ranked"] = ranked_totals
+        context["total_electoral_points"] = total_electoral_points
+        context["winning_electoral_points"] = winning_electoral_points
+        context["ranked_votes"] = ranked_votes
+        context["colors"] = colors
+        context["candidates"] = candidates
+        context["electors"] = electors
+        context["template_path"] = f'electoral/imgs/{kwargs["year"]}_{kwargs["position"]}_img.html'
+                
+        return context
